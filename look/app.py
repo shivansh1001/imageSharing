8da33d79ac5609e5a385120642bc5d6bd174315b
shivansh <shivansh1001@gmail.com>
2018-06-11 01:01:29 +0530

    refactoring code for GET POST request done

--- look/app.py
+++ look/app.py
@@ -1,8 +1,12 @@
 import falcon
+from .images import Resource, ImageStore
 
-from .images import Resource
+def create_app(image_store):
+    image_resource = Resource(image_store)
+    api= falcon.API()
+    api.add_route('/images',image_resource)
+    return api
 
-api = application = falcon.API()
-
-images = Resource(storage_path='.')
-api.add_route('/images',images)
+def get_app():
+    image_store = ImageStore(',')
+    return create_app((image_store)
